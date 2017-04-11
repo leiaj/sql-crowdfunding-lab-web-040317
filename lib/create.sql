4a7a905122f931c8e4a9f69c1a8162b139a5006e
github username <github email address>
2017-04-11 11:40:38 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,29 @@
+-- CREATE TABLE cats (
+-- id INTEGER PRIMARY KEY,
+-- name TEXT,
+-- age INTEGER,
+-- breed TEXT,
+-- net_worth INTEGER
+-- );
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
